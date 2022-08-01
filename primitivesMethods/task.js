c1d0e9046ac9eed70e0a1fca5d6fbf4b9dfad037
AleksiDu <aleksi.duluzauri@gmail.com>
2022-08-01 09:30:53 +0400

    Data types: Primitives methods task

---
+++ primitivesMethods/task.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * Can i add a string property?
+ */
+
+let str = "Hello";
+str.test = 5;
+console.log(str.test);
+
+// This example clearly shows that orimitives are not objects.
