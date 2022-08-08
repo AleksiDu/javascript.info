78dc2fd9e3eee33f9654c8231439c7280d4fe602
AleksiDu <aleksi.duluzauri@gmail.com>
2022-08-08 11:41:36 +0400

    Data types; numbers-math functions

--- numbers/script.js
+++ numbers/script.js
@@ -106,8 +106,46 @@ console.log(isNaN(NaN)); //true
 console.log(isNaN("str")); // true
 console.log(NaN === NaN); // false
 
-// isFinite(value) converts its argument to a number and returns true if ti's a regular number, not NaN/Infinity/-Infinity
+// isFinite(value) converts its argument to a number and returns true if it's a regular number, not NaN/Infinity/-Infinity
 console.log(isFinite("15")); // true
 console.log(isFinite("str")); // false, because a special value: NaN
 console.log(isFinite(Infinity)); // false, because a special value: Infinity
+// Sometimes isFinite is used to validate whether a string is regular number:
+num = +"10";
+console.log("isFinite(num) =>", isFinite(num));
 
+/**
+ * parseInt and parseFloat
+ * Numeric conversion using a plus + or Number() is strict. If a value is not exactly a number, it fails:
+ */
+console.log(+"100px"); //NaN
+
+console.log("parseInt('100px') =>", parseInt('100px')); // 100
+console.log("parseFloat('12.5em') =>", parseFloat('12.5em')); // 12.5
+console.log("parseInt('12.3') =>", parseInt('12.3')) // 12, only the integer part is returned
+console.log("parseFloat('12.3.4') =>", parseFloat('12.3.4')); //12.3, the second point stops the reading
+// There are situation when parseInt/parseFloat will return NaN. It happens whem mo digits could be read:
+console.log("parseInt('a123') =>", parseInt('a123')); //NaN, the first symbol stops the process
+console.log("parseFloat('.123') =>", parseInt('.123')); //NaN, the first symbol stops the process
+
+// The second argument of parseInt(str, radix)
+/*The parseInt() function has an optional second parameter.
+ It specifies the base of the numeral system, so parseInt can also parse strings of hex numbers,
+ binary numbers and so on:
+ */
+console.log(parseInt('0xff', 16)); //255, without 0x also works
+
+/**
+ * Other math functions
+ */
+
+// Math.random()
+console.log(Math.random()); // any random numbers
+
+// Math.max(a, b, c...) and Math.min(a, b, c...)
+console.log(Math.max(3, 4, -10, 0, 1)); //4
+console.log(Math.min(1, 2)); //1
+
+// Math.pow(n, power)
+// Returns n raised to given power.
+console.log(Math.pow(2, 10)); // 2 in power 10 = 1024
