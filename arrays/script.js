44061e87d8bc4c7e4e7621c17a7222d1b170df75
AleksiDu <aleksi.duluzauri@gmail.com>
2022-08-15 19:35:45 +0400

    Data types: Arrays => Declaration

--- arrays/script.js
+++ arrays/script.js
@@ -1,2 +1,18 @@
 "use strict";
 
+/**
+ * Declaration
+ */
+
+// There are two syntaxes for creating an empty array:
+
+let arr = new Array();
+arr = [];
+
+let fruits = ["Apple", "Orange", "Plum"];
+
+// We can get an element by its number in square brackets:
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
